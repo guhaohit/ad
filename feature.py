5591f1108ec088e3fe2d55471bd9ef29e236bf60
guhao <guhaohit@gmail.com>
2014-09-11 21:55:12 +0800

    2014-09-11

--- feature.py
+++ feature.py
@@ -1,16 +1,26 @@
 import csv
+from numpy import *
 
 def labeledit():
 	reader = csv.reader(open("Downloads/train.csv"))
 	trainad = []
-	featurelen = []
+	labelfeature = []
 	for i in range(26):
 		featurec1 = set([])
 		for line in reader:
 			trainad.append(line[i+15])
 		featurec1 = set(trainad)
-		featurelen.append(len(featurec1))
-	return featurelen
+		for j in range(len(trainad)):
+			trainad[j] = list(featurec1).index(trainad[j])
+		labelfeature.append(trainad)
+	labelarray = array(labelfeature)
+	labelfeature = list(labelarray.T)
+	csvfile = file('label_feature.csv', 'wb')
+	writer = csv.writer(csvfile)
+
+	writer.writerows(labelfeature)
+
+	csvfile.close()
 
 def haha():
-	return 2
+	return 3
